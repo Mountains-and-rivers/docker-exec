a0f84d53a121482c55f6d74b7d7909036b280149
Mountains-and-rivers <qzrkqzry2017@gmail.com>
2021-11-15 00:00:07 +0800

    1

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/ahmetalpbalkan/dexec v0.0.0-20160630173852-2538e5e2198f
 	github.com/ahmetalpbalkan/go-dexec v0.0.0-20160630173852-2538e5e2198f // indirect
 	github.com/fsouza/go-dockerclient v1.7.4
+	github.com/mileusna/crontab v1.2.0
 )
--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
-
+	"github.com/mileusna/crontab"
 	"github.com/ahmetalpbalkan/dexec"
 	"github.com/fsouza/go-dockerclient"
 )
 
-func main(){
+func test(){
 	cl, _ := docker.NewClient("unix:///run/docker.sock")
 	d := dexec.Docker{cl}
 
@@ -18,4 +19,27 @@ func main(){
 	b, err := cmd.Output()
 	if err != nil { log.Fatal(err) }
 	log.Printf("%s", b)
+}
+func main() {
+	ctab := crontab.New()
+	err := ctab.AddJob("*/1 * * * *", myFunc)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	select {
+
+	}
+}
+
+func myFunc() {
+	fmt.Println("Helo, world")
+}
+
+func myFunc3() {
+	fmt.Println("Noon!")
+}
+
+func myFunc2(s string, n int) {
+	fmt.Println("We have params here, string", s, "and number", n)
 }
