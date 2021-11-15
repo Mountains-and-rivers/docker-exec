f55b448d52ee3847426aa2af835e474b457dda5f
Mountains-and-rivers <qzrkqzry2017@gmail.com>
2021-11-15 23:55:17 +0800

    1

--- go.mod
+++ go.mod
@@ -3,8 +3,6 @@ module docker-exec
 go 1.16
 
 require (
-	github.com/ahmetalpbalkan/dexec v0.0.0-20160630173852-2538e5e2198f
-	github.com/ahmetalpbalkan/go-dexec v0.0.0-20160630173852-2538e5e2198f // indirect
 	github.com/fsouza/go-dockerclient v1.7.4
 	github.com/mileusna/crontab v1.2.0
 )
--- main.go
+++ main.go
@@ -1,45 +1,129 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"sync"
 	"github.com/mileusna/crontab"
-	"github.com/ahmetalpbalkan/dexec"
 	"github.com/fsouza/go-dockerclient"
 )
 
-func test(){
-	cl, _ := docker.NewClient("unix:///run/docker.sock")
-	d := dexec.Docker{cl}
-
-	m, _ := dexec.ByCreatingContainer(docker.CreateContainerOptions{
-		Config: &docker.Config{Image: "8c811b4aec35"}})
-
-	cmd := d.Command(m, "ifconfig", `eth0`)
-	b, err := cmd.Output()
-	if err != nil { log.Fatal(err) }
-	log.Printf("%s", b)
-}
 func main() {
+	//ctx := context.Background()
 	ctab := crontab.New()
-	err := ctab.AddJob("*/1 * * * *", myFunc)
+	err := ctab.AddJob("*/1 * * * *", expireSession)
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	select {
+	select {}
+}
 
+func expireSession(){
+	commad := `
+#!/bin/bash
+docker ps|grep "k8s_nginx_nginx"|awk '{print $1}'
+`
+	id,err:=getExecOut(commad)
+	if err != nil {
+		log.Fatalf("cmd.Start() failed with '%s'\n", err)
+	}
+
+	execInContainer(strings.Trim(id,"\n"),"/usr/sbin/nginx -help")
+}
+
+func execInContainer(id string,cmd string){
+	client, _ := docker.NewClient(`unix:///run/docker.sock`)
+	command := []string{"/bin/bash", "-c", cmd}
+
+	exec, err := client.CreateExec(docker.CreateExecOptions{
+		AttachStderr: true,
+		AttachStdin:  false,
+		AttachStdout: true,
+		Tty:          false,
+		Cmd:          command,
+		Container:    id,
+	})
+
+	if err != nil {
+		log.Println(err)
+	}
+	var outputBytes []byte
+	outputWriter := bytes.NewBuffer(outputBytes)
+	var errorBytes []byte
+	errorWriter := bytes.NewBuffer(errorBytes)
+
+	err = client.StartExec(exec.ID, docker.StartExecOptions{
+		OutputStream: outputWriter,
+		ErrorStream:  errorWriter,
+	})
+	log.Println(outputWriter.String())
+	log.Println(errorWriter.String())
+	log.Println(err)
+}
+func copyAndCapture(w io.Writer, r io.Reader) ([]byte, error) {
+	var out []byte
+	buf := make([]byte, 1024, 1024)
+	for {
+		n, err := r.Read(buf[:])
+		if n > 0 {
+			d := buf[:n]
+			out = append(out, d...)
+			_, err := w.Write(d)
+			if err != nil {
+				return out, err
+			}
+		}
+		if err != nil {
+			// Read returns io.EOF at the end of file, which is not an error for us
+			if err == io.EOF {
+				err = nil
+			}
+			return out, err
+		}
 	}
 }
 
-func myFunc() {
-	fmt.Println("Helo, world")
-}
+func getExecOut(commad string) (string,error) {
+	cmd := exec.Command("/bin/bash", "-c",commad)
+	var stdout, stderr []byte
+	var errStdout, errStderr error
+	stdoutIn, _ := cmd.StdoutPipe()
+	stderrIn, _ := cmd.StderrPipe()
+	err := cmd.Start()
+	if err != nil {
+		log.Fatalf("cmd.Start() failed with '%s'\n", err)
+		return "",err
+	}
+	// cmd.Wait() should be called only after we finish reading
+	// from stdoutIn and stderrIn.
+	// wg ensures that we finish
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		stdout, errStdout = copyAndCapture(os.Stdout, stdoutIn)
+		wg.Done()
+	}()
 
-func myFunc3() {
-	fmt.Println("Noon!")
-}
+	stderr, errStderr = copyAndCapture(os.Stderr, stderrIn)
 
-func myFunc2(s string, n int) {
-	fmt.Println("We have params here, string", s, "and number", n)
+	wg.Wait()
+
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatalf("cmd.Run() failed with %s\n", err)
+		return "",err
+	}
+	if errStdout != nil || errStderr != nil {
+		log.Fatal("failed to capture stdout or stderr\n")
+		return "",err
+	}
+	out, errStr := string(stdout), string(stderr)
+	fmt.Printf("\nout:\n%s\nerr:\n%s\n", out, errStr)
+	return out,err
 }
